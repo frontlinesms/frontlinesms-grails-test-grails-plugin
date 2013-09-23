b9b1fcdb8e93544d3880a10ebb53da172c97a64f
Alex Anderson <alex@frontlinesms.com>
2013-09-23 12:05:40 +0300

    [RELEASE: 0.12]

--- FrontlinesmsGrailsTestGrailsPlugin.groovy
+++ FrontlinesmsGrailsTestGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class FrontlinesmsGrailsTestGrailsPlugin {
-	def version = '0.12-SNAPSHOT'
+	def version = '0.12'
 	def grailsVersion = '2.0 > *'
 	def title = 'Frontlinesms Test Plugin'
 	def author = 'Alex Anderson'
