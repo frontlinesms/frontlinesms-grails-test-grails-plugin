18f0ca3870acc8b49fa25700aefa72724907bfc1
ivan <ivanoronee@gmail.com>
2014-10-02 16:51:23 +0300

    CORE-2291:Rolled up version for release after selenium plugin upgrade

--- FrontlinesmsGrailsTestGrailsPlugin.groovy
+++ FrontlinesmsGrailsTestGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import frontlinesms.grails.test.EchoMessageSource
 import grails.util.Environment
 
 class FrontlinesmsGrailsTestGrailsPlugin {
-	def version = '0.19'
+	def version = '0.20'
 	def grailsVersion = '2.0 > *'
 	def title = 'Frontlinesms Test Plugin'
 	def author = 'Alex Anderson'
