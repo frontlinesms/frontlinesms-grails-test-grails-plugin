24c700d9e7db13d1298000636c10a1f041f16b78
fredotieno <otieno.alfred@gmail.com>
2016-04-18 12:12:17 +0300

    CORE-3178 rollup version to in dev snapshot

--- FrontlinesmsGrailsTestGrailsPlugin.groovy
+++ FrontlinesmsGrailsTestGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import frontlinesms.grails.test.EchoMessageSource
 import grails.util.Environment
 
 class FrontlinesmsGrailsTestGrailsPlugin {
-	def version = '0.24'
+	def version = '0.25-SNAPSHOT'
 	def grailsVersion = '2.0 > *'
 	def title = 'Frontlinesms Test Plugin'
 	def author = 'Alex Anderson'
